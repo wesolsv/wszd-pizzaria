64188269afa821b94bb15d1cf50d5d8eb076b429
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-07-08 12:54:29 -0300

    incluindo e removendo itens do pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,6 +11,8 @@ import uploadConfig from './config/multer'
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
 
 
 const router = Router();
@@ -38,6 +40,7 @@ router.get('/category/product',isAuthenticated, new ListByCategoryController().h
 router.post('/order',isAuthenticated, new CreateOrderController().handle)
 router.delete('/order',isAuthenticated, new RemoveOrderController().handle)
 
-
+router.post('/order/add',isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove',isAuthenticated, new RemoveItemController().handle)
 
 export { router };
