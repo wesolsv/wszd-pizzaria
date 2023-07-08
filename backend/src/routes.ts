84e9a3b8a721c27d3ed124d41847c758e272b78a
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-07-08 10:43:35 -0300

    incluindo cadastro de categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
 
 
@@ -13,6 +14,9 @@ router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+//--Rotas Categorie--
+router.post('/category',isAuthenticated, new CreateCategoryController().handle)
 
 
 export { router };
