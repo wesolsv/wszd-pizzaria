4fc7bf2b796e1fa4e416f226a82d9e333b7b9776
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-07-08 13:54:46 -0300

    enviando o pedido e listando pedidos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -13,6 +13,8 @@ import { CreateOrderController } from './controllers/order/CreateOrderController
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrdersController } from './controllers/order/ListOrdersController';
 
 
 const router = Router();
@@ -42,5 +44,9 @@ router.delete('/order',isAuthenticated, new RemoveOrderController().handle)
 
 router.post('/order/add',isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove',isAuthenticated, new RemoveItemController().handle)
+
+router.put('/order/send',isAuthenticated, new SendOrderController().handle)
+
+router.get('/orders',isAuthenticated, new ListOrdersController().handle)
 
 export { router };
