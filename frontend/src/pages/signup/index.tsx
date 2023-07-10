2ce11d78d7fb473558f794f30d1d1bf14550c823
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-07-10 19:28:03 -0300

    criando tela de cadastro

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,33 +1,37 @@
 import Head from "next/head";
 import Image from "next/image";
-import logoImg from '../../public/logo.svg';
-import styles from '../../styles/home.module.scss';
+import logoImg from '../../../public/logo.svg';
+import styles from '../../../styles/home.module.scss';
+
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>Pizzaria Wszd</title>
+        <title>Faça seu cadastro agora</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo pizzaria" />
         <div className={styles.login}>
+            <h1>Criando sua conta</h1>
           <form>
+
+            <Input placeholder="Digite seu nome" type="text" /> 
 
             <Input placeholder="Digite seu email" type="text" />
 
             <Input placeholder="Sua senha" type="password" />
 
             <Button type="submit" loading={false}>
-              Acessar
+              Cadastrar
             </Button>
 
           </form>
-          <Link href={"/signup"} className={styles.text}>
-            Não possui uma conta? Cadastre-se
+          <Link href={"/"} className={styles.text}>
+            Já possui uma conta? Faça Login
           </Link>
         </div>
       </div>
