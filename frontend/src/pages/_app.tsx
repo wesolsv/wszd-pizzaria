8b7d4c0dfc129e78bf221543e5026ba2df0c846c
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-07-10 19:51:50 -0300

    criando contexto para login

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import '../../styles/globals.scss'
 import { AppProps } from "next/app";
+import { AuthProvider } from '@/contexts/AuthContext';
 
 
-function MyApp({Component, pageProps}: AppProps){
-    return <Component {...pageProps}/>
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <AuthProvider>
+            <Component {...pageProps} />
+        </AuthProvider>
+    )
 }
 
 export default MyApp
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useContext, FormEvent } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import logoImg from '../../public/logo.svg';
@@ -5,8 +6,19 @@ import styles from '../../styles/home.module.scss';
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 import Link from "next/link";
+import { AuthContext } from "@/contexts/AuthContext";
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  async function handleLogin(event: FormEvent ) {
+    event.preventDefault();
+    let data = {
+      email: "teste@teste.com",
+      password: "123123"
+    }
+    await signIn(data)
+  }
   return (
     <>
       <Head>
@@ -15,7 +27,7 @@ export default function Home() {
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo pizzaria" />
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
 
             <Input placeholder="Digite seu email" type="text" />
 
