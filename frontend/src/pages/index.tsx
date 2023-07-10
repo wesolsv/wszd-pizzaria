81178969ca54dfc686ed1a4ed4359e98dc750594
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-07-10 19:19:19 -0300

    finalizando tela de login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import logoImg from '../../public/logo.svg';
-import styles from '../../styles/Home.module.scss';
+import styles from '../../styles/home.module.scss';
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 
@@ -14,13 +14,19 @@ export default function Home() {
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo pizzaria" />
         <div className={styles.login}>
-          <form action="">
+          <form>
+
             <Input placeholder="Digite seu email" type="text" />
+
             <Input placeholder="Sua senha" type="password" />
+
             <Button type="submit" loading={false}>
               Acessar
             </Button>
+
           </form>
+
+          <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
         </div>
       </div>
     </>
