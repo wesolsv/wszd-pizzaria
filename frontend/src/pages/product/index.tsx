75ff0fe733894d35803fba304c1bc72272e46b3b
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-07-16 11:31:00 -0300

    criando busca de categorias

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,42 +1,105 @@
+import { useState, ChangeEvent } from 'react'
 import Head from "next/head";
 import styles from './styles.module.scss'
 import { canSSRAuth } from "@/utils/canSSRAuth";
 import { Header } from "@/components/Header";
+import { FiUpload } from "react-icons/fi";
+import { setupAPIClient } from '@/services/api';
 
-export default function Product() {
+type ItemProps = {
+  id: string,
+  name: string
+}
+
+interface CategoryProps {
+  categoryList: ItemProps[];
+}
+
+export default function Product({ categoryList }: CategoryProps) {
+
+  const [avatarUrl, setAvatarUrl] = useState('');
+  const [imageAvatar, setImageAvatar] = useState(null);
+
+  const [categories, setCategories] = useState(categoryList || [])
+  const [categorySelected, setCategorySelected] = useState(0)
+
+  function handleFile(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files) {
+      return;
+    }
+
+    const image = event.target.files[0];
+
+    if (!image) {
+      return;
+    }
+
+    if (image.type === 'image/jpeg' || image.type === 'image/png') {
+      setImageAvatar(image);
+      setAvatarUrl(URL.createObjectURL(event.target.files[0]))
+    }
+  }
+
+  function handleChangeCategory(event){
+    setCategorySelected(event.target.value)
+  }
+
   return (
     <>
       <Head>
         <title>Novo produto - Wszd Pizzaria</title>
       </Head>
       <div>
-        <Header/>
+        <Header />
         <main className={styles.container}>
           <h1>Novo Produto</h1>
 
           <form className={styles.form}>
-            <select>
-              <option>
-                Bebidas
-              </option>
-              <option>
-                Pizzas
-              </option>
+
+            <label className={styles.labelAvatar}>
+              <span>
+                <FiUpload size={30} color="#FFF" />
+              </span>
+
+              <input
+                type="file"
+                accept="image/png, image/jpeg"
+                onChange={handleFile}
+              />
+
+              {avatarUrl && (
+                <img
+                  className={styles.preview}
+                  src={avatarUrl}
+                  alt="Foto do produto"
+                  width={250}
+                  height={250}
+                />
+              )}
+            </label>
+            <select value={categorySelected} onChange={handleChangeCategory}>
+              {categories.map((item, index) => {
+                return (
+                  <option key={item.id} value={index}>
+                    {item.name}
+                  </option>
+                )
+              })}
             </select>
 
-            <input 
-              type="text" 
+            <input
+              type="text"
               placeholder="Digite o nome do Produto"
               className={styles.input}
             />
 
-            <input 
-              type="text" 
+            <input
+              type="text"
               placeholder="Digite o preço"
               className={styles.input}
             />
 
-            <textarea 
+            <textarea
               placeholder="Digite o preço"
               className={styles.input}
             />
@@ -45,14 +108,20 @@ export default function Product() {
               Cadastrar
             </button>
           </form>
-        </main>        
+        </main>
       </div>
     </>
   )
 }
 
-export const getServerSideProps = canSSRAuth(async (ctx) =>{
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx)
+
+  const response = await apiClient.get('/category');
+
   return {
-    props: {}
+    props: {
+      categoryList: response.data
+    }
   }
 })
