d91c3750d98801f1108d6c84a39d0bada902eeb8
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-10-09 14:38:11 -0300

    incluindo itens na lista

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -6,11 +6,13 @@ import {
   TouchableOpacity,
   TextInput,
   Modal,
+  FlatList
 } from 'react-native';
 import { useRoute, RouteProp, useNavigation } from '@react-navigation/native'
 import { Feather } from '@expo/vector-icons'
 import { ModalPicker } from '../../components/ModalPicker';
 import { api } from '../../services/api';
+import { ListItem } from '../../components/ListItem';
 
 type RouteDetailParams = {
   Order: {
@@ -29,6 +31,13 @@ export type ProductProps = {
   name: string;
 }
 
+type ItemProps = {
+  id: string;
+  product_id: string;
+  name: string;
+  amount: string | number;
+}
+
 type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>;
 
 export default function Order() {
@@ -40,10 +49,11 @@ export default function Order() {
   const [modalCategoryVisible, setModalCategoryVisible] = useState(false)
 
   const [products, setProducts] = useState<ProductProps[] | []>([]);
-  const [procutSelected, setProductSelected] = useState<ProductProps | undefined>()
+  const [productSelected, setProductSelected] = useState<ProductProps | undefined>()
   const [modalProductVisible, setModalProductVisible] = useState(false)
 
   const [amount, setAmount] = useState('1')
+  const [items, setItems] = useState<ItemProps[]>([])
 
   useEffect(() => {
     async function loadInfo() {
@@ -89,8 +99,26 @@ export default function Order() {
     setCategorySelected(item);
   }
 
-  function handleChangeProduct(item: ProductProps){
+  function handleChangeProduct(item: ProductProps) {
     setProductSelected(item);
+  }
+
+  // adicionar produto na mesa
+  async function handleAdd() {
+    const response = await api.post('/order/add', {
+      order_id: route.params.order_id,
+      product_id: productSelected?.id,
+      amount: Number(amount)
+    })
+
+    let data = {
+      id: response.data.id,
+      product_id: productSelected?.id as string,
+      name: productSelected?.name as string,
+      amount: amount
+    }
+
+    setItems(oldArray => [...oldArray, data])
   }
 
   return (
@@ -98,9 +126,10 @@ export default function Order() {
 
       <View style={styles.header}>
         <Text style={styles.title}>Mesa {route.params.number}</Text>
-        <TouchableOpacity onPress={handleCloseOrder}>
-          <Feather name='trash-2' size={28} color="#FF3F4b" />
-        </TouchableOpacity>
+        {items.length === 0 && (
+          <TouchableOpacity onPress={handleCloseOrder}>
+            <Feather name='trash-2' size={28} color="#FF3F4b" />
+          </TouchableOpacity>)}
       </View>
 
       {category.length !== 0 && (
@@ -114,7 +143,7 @@ export default function Order() {
       {products.length !== 0 && (
         <TouchableOpacity style={styles.input} onPress={() => setModalProductVisible(true)}>
           <Text style={{ color: '#FFF' }}>
-            {procutSelected?.name}
+            {productSelected?.name}
           </Text>
         </TouchableOpacity>
       )}
@@ -131,14 +160,24 @@ export default function Order() {
       </View>
 
       <View style={styles.actions}>
-        <TouchableOpacity style={styles.buttonAdd}>
+        <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={[styles.button, { opacity: items.length === 0 ? 0.3 : 1 }]}
+          disabled={items.length === 0}>
           <Text>Avançar</Text>
         </TouchableOpacity>
       </View>
+
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        style={{ flex: 1, marginTop: 24 }}
+        data={items}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <ListItem data={item} />}
+      />
 
       <Modal
         transparent={true}
